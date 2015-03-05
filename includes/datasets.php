4173a235c81eef4857bfe929f3a0a12e37015992
noxwyll <wyll@live.de>
2015-03-05 14:08:14 +0100

    Update datasets.php
    roman numerals

--- includes/datasets.php
+++ includes/datasets.php
@@ -1,5 +1,36 @@
 <?php
 //
+//Curse you oh roman numerals
+//
+$romnum = array(
+	1 => 'I',
+	2 => 'II',
+	3 => 'III',
+	4 => 'IV',
+	5 => 'V',
+	6 => 'VI',
+	7 => 'VII',
+	8 => 'VIII',
+	9 => 'IX',
+	10 => 'X',
+	11 => 'XI',
+	12 => 'XII',
+	13 => 'XIII',
+	14 => 'XIV',
+	15 => 'XV',
+	16 => 'XVI',
+	17 => 'XVII',
+	18 => 'XVIII',
+	19 => 'XIX',
+	20 => 'XX',
+	21 => 'XXI',
+	22 => 'XXII',
+	23 => 'XXIII',
+	24 => 'XXIV',
+	25 => 'XXV'
+);
+
+//
 //PRIMARY
 //
 $weapons_primary = array(
